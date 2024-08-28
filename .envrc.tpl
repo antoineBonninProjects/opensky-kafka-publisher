b2d53a97edf746d7580c092564e8ff75e2d51098
Antoine Bonnin <bonnin.anto@gmail.com>
2024-08-28 08:32:03 +0200

    (variabilization): use env variables to define the area to scan

--- .envrc.tpl
+++ .envrc.tpl
@@ -2,3 +2,10 @@ export DOCKER_USERNAME="your-username"
 export DOCKER_PASSWORD="your-password"
 
 export IS_DRY_RUN=true
+export KAFKA_BROKER=kafka-service:9092
+export KAFKA_TOPIC=aircraft-states
+
+export LATITUDE_MIN=42.5
+export LATITUDE_MAX=45
+export LONGITUDE_MIN=-1.5
+export LONGITUDE_MAX=1.5
--- src/main/java/com/opensky_puller/OpenSkyQuery.java
+++ src/main/java/com/opensky_puller/OpenSkyQuery.java
@@ -19,9 +19,20 @@ import org.json.JSONObject;
 
 public class OpenSkyQuery {
 
-    private static final String KAFKA_BROKER = "kafka-service:9092";
-    private static final String TOPIC = "aircraft-states";
+    private static final String KAFKA_BROKER = System.getenv("KAFKA_BROKER");
+    private static final String KAFKA_TOPIC = System.getenv("KAFKA_TOPIC");
     private static final String IS_DRY_RUN = System.getenv("IS_DRY_RUN");
+
+    private static final String LATITUDE_MIN = System.getenv("LATITUDE_MIN");
+    private static final String LATITUDE_MAX = System.getenv("LATITUDE_MAX");
+    private static final String LONGITUDE_MIN = System.getenv("LONGITUDE_MIN");
+    private static final String LONGITUDE_MAX = System.getenv("LONGITUDE_MAX");
+
+    // Default area to scan is the south west of France
+    private static final Double DEFAULT_LATITUDE_MIN = 42.5;
+    private static final Double DEFAULT_LATITUDE_MAX = 45.0;
+    private static final Double DEFAULT_LONGITUDE_MIN = -1.5;
+    private static final Double DEFAULT_LONGITUDE_MAX = 1.5;
 
     public static void main(String[] args) {
 
@@ -40,10 +51,14 @@ public class OpenSkyQuery {
         }
 
         try {
-            // Perform the HTTP query and get the response
+            // Scan for aircrafts and handle the response
             JSONObject jsonObject = performHttpQuery();
 
-            if (jsonObject != null) {
+            if (jsonObject == null) {
+                System.out.println("Failed to fetch data.");
+            } else if (!jsonObject.has("states") || jsonObject.isNull("states")) {
+                System.out.println("No aircrafts in the scanned zone.");
+            } else  {
                 JSONArray statesArray = jsonObject.getJSONArray("states");
                 Long time = jsonObject.getLong("time");
 
@@ -61,15 +76,13 @@ public class OpenSkyQuery {
 
                     // If not in dry-run mode, send the message to Kafka
                     if (!isDryRun) {
-                        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC, key, message);
+                        ProducerRecord<String, String> record = new ProducerRecord<>(KAFKA_TOPIC, key, message);
                         Future<RecordMetadata> future = producer.send(record);
                         RecordMetadata metadata = future.get();
                         System.out.printf("Produced record to topic %s partition %d with offset %d%n",
                             metadata.topic(), metadata.partition(), metadata.offset());
                     }
                 }
-            } else {
-                System.out.println("Failed to fetch data.");
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -84,10 +97,17 @@ public class OpenSkyQuery {
     private static JSONObject performHttpQuery() throws Exception {
         HttpClient client = HttpClient.newHttpClient();
 
-        Double lamin = 42.5;
-        Double lomin = -1.5;
-        Double lamax = 45.0;
-        Double lomax = 1.5;
+        Double lamin = (LATITUDE_MIN != null) ? Double.parseDouble(LATITUDE_MIN) : DEFAULT_LATITUDE_MIN;
+        Double lomin = (LONGITUDE_MIN) != null ? Double.parseDouble(LONGITUDE_MIN) : DEFAULT_LONGITUDE_MIN;
+        Double lamax = (LATITUDE_MAX) != null ? Double.parseDouble(LATITUDE_MAX) : DEFAULT_LATITUDE_MAX;
+        Double lomax = (LONGITUDE_MAX) != null ? Double.parseDouble(LONGITUDE_MAX) : DEFAULT_LONGITUDE_MAX;
+
+        System.out.println("Area to scan is: ");
+        System.out.println("\tLATITUDE_MIN: " + lamin);
+        System.out.println("\tLATITUDE_MAX: " + lamax);
+        System.out.println("\tLONGITUDE_MIN: " + lomin);
+        System.out.println("\tLONGITUDE_MAX: " + lomax);
+
         String url = String.format(
             "https://opensky-network.org/api/states/all?lamin=%.1f&lomin=%.1f&lamax=%.1f&lomax=%.1f", 
             lamin, lomin, lamax, lomax
